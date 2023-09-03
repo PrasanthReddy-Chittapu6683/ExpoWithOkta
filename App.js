16be4e9de283741d1eb44a2a5a8a2a853e7fab11
Prasanth Reddy CV <prasanthreddy.chittapu@gmail.com>
2023-09-03 23:51:27 +0530

    updating okta code

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Button, Platform } from 'react-native';
 // import Auth from './Auth';
 import configFile from './auth.config';
 import * as WebBrowser from 'expo-web-browser';
-import { makeRedirectUri, useAuthRequest, useAutoDiscovery } from 'expo-auth-session';
+import { makeRedirectUri, TokenResponse, useAuthRequest, useAutoDiscovery } from 'expo-auth-session';
 
 
 WebBrowser.maybeCompleteAuthSession()
@@ -19,24 +19,37 @@ export default function App() {
     {
       clientId: configFile.oidc.clientId,
       scopes: configFile.oidc.scopes,
+      responseType: "token",
+      extraParams: {
+        nonce: "foo"
+      },
+
 
       // For usage in managed apps using the proxy
-      redirectUri: makeRedirectUri({
-        // For usage in bare and standalone
-        native: configFile.oidc.redirectUri,
-        useProxy,
-      }),
+      redirectUri: configFile.oidc.redirectUri,
+      //     makeRedirectUri({
+      //   // For usage in bare and standalone
+      //   native: configFile.oidc.redirectUri,
+      //   useProxy,
+      // }),
     },
     discovery
   );
 
+  console.log("request >>>", request);
+
   React.useEffect(() => {
-    console.log("ONE >>>", response)
+
+    console.log("ONE >>>", response);
+
+    // const token = TokenResponse.fromQueryParams(response.params);
+    // console.log("TokenResponse >>>", token)
+
     if (response?.type === 'success') {
       console.log("TWO >>>", response)
-
       const { code } = response.params;
     }
+
     setloginCode(response?.type)
   }, [response]);
 
@@ -51,6 +64,8 @@ export default function App() {
           promptAsync({ useProxy });
         }}
       />
+
+
       <Text>Response : {loginCode}</Text>
       <StatusBar style="auto" />
     </View>
--- auth.config.js
+++ auth.config.js
@@ -1,9 +1,53 @@
+// React native Apollo
+// export default {
+//     oidc: {
+//         clientId: '0oa9rycrsvjRbv1k21d7',
+//         redirectUri: 'com.oktapreview.apolloid:/callback',
+//
+//         endSessionRedirectUri: 'http://localhost:8082/logout',
+//         issuer: 'https://apolloid.oktapreview.com/oauth2/default',
+//         scopes: ['openid', 'profile', 'offline_access'],
+//     },
+// };
+
+
+// Apollo Web
 export default {
     oidc: {
         clientId: '0oa5qqn5zbXK76rcf1d7',
-        redirectUri: 'https://apollo-g9ejbqeve8a7b7e01.azurefd.net/authorization-code/callback',
-        endSessionRedirectUri: 'com.oktapreview.apolloid:/logout',
+        redirectUri: 'com.oktapreview.apolloid:/callback',
+        endSessionRedirectUri: 'http://localhost:8082/logout',
         issuer: 'https://apolloid.oktapreview.com/oauth2/default',
         scopes: ['openid', 'profile', 'offline_access'],
     },
 };
+
+// export default {
+//     oidc: {
+//         clientId: '0oab05a0eavoP0Ox75d7',
+//         redirectionURL: 'http://10.0.0.172:8082/authorization-code/callback',
+//         endSessionRedirectUri: 'http://localhost:8082/logout',
+//         issuer: 'https://dev-70352772.okta.com/oauth2/default',
+//         scopes: ['openid', 'profile', 'offline_access'],
+//     },
+// };
+
+// export default {
+//     oidc: {
+//         clientId: '0oab16kgzxXhoYWt45d7',
+//         redirectUri: 'http://10.0.0.172:8082/authorization-code/callback',
+//         endSessionRedirectUri: 'http://10.0.0.172:8082/logout',
+//         issuer: 'https://dev-70352772.okta.com/oauth2/default',
+//         scopes: ['openid', 'profile', 'offline_access'],
+//     },
+// };
+
+// export default {
+//     oidc: {
+//         clientId: '0oa5qqn5zbXK76rcf1d7',
+//         redirectUri: 'https://apollo-g9ejbqeve8a7b7e01.azurefd.net/authorization-code/callback',
+//         endSessionRedirectUri: 'com.oktapreview.apolloid:/logout',
+//         issuer: 'https://apolloid.oktapreview.com/oauth2/default',
+//         scopes: ['openid', 'profile', 'offline_access'],
+//     },
+// };
